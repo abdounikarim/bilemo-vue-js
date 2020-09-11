d98f8e602abc3e0ab3ccb268b563b53c307df392
abdounikarim <abdounikarim@gmail.com>
2020-09-11 10:36:52 +0200

    work on phones page

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/", name="front")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/phones", name="phones")
+     */
+    public function phones(PhoneRepository $phoneRepository)
+    {
+        return $this->render('front/phones.html.twig', [
+            'phones' => $phoneRepository->findAll()
+        ]);
     }
 }
